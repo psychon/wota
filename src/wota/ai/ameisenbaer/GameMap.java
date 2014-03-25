44ef5096f0aec5c8158b1e268284d2b7e5603365
Uli Schlachter <psychon@znc.in>
2014-03-25 21:26:38 +0100

    GameMap: Refactoring
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- src/wota/ai/ameisenbaer/GameMap.java
+++ src/wota/ai/ameisenbaer/GameMap.java
@@ -8,12 +8,15 @@ import wota.utility.Vector;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
 
 public class GameMap {
-	private int tick = 0;
+	private int ticks = 0;
 	private SnapshotList<Sugar> sugarSeen = new SnapshotList<>();
-	private int sugarSeenMessageIndex = 0;
+	private Queue<Seen<? extends Snapshot>> broadcastList = null;
 
 	private final InViewInterface inView;
 
@@ -45,10 +48,29 @@ public class GameMap {
 		public boolean hasSameOriginal(U other) {
 			return snapshot.hasSameOriginal(other);
 		}
+
+		public Action getAction(int ticks) {
+			int t = ticks - this.tick;
+			if (!this.alive)
+				t *= -1;
+			return new Action(t, this.snapshot);
+		}
+
+		public void updateFromMessage(int content) {
+			if (content <= 0)
+				alive = false;
+		}
+
+		static Seen<Sugar> sugarSeenFromMessage(int ticks, int content, Sugar snapshot) {
+			if (content <= 0)
+				return new Seen<Sugar>(snapshot, ticks + content, false);
+			else
+				return new Seen<Sugar>(snapshot, ticks - content);
+		}
 	}
 
 	private static class SnapshotList<T extends Snapshot> implements Iterable<Seen<T>> {
-		public final List<Seen<T>> snapshots = new LinkedList<>();
+		public final List<Seen<T>> snapshots = new ArrayList<>();
 
 		@Override
 		public Iterator<Seen<T>> iterator() {
@@ -56,7 +78,6 @@ public class GameMap {
 		}
 
 		private Iterator<Seen<T>> findEntry(T snapshot) {
-			// Remove old entry, if one exists
 			Iterator<Seen<T>> it = snapshots.iterator();
 			while (it.hasNext()) {
 				Seen<T> t = it.next();
@@ -68,11 +89,20 @@ public class GameMap {
 			return null;
 		}
 
-		private boolean isKnown(T snapshot) {
-			return findEntry(snapshot) != null;
+		private Seen<T> getEntry(T snapshot) {
+			Iterator<Seen<T>> it = snapshots.iterator();
+			while (it.hasNext()) {
+				Seen<T> t = it.next();
+				if (!snapshot.hasSameOriginal(t.snapshot))
+					continue;
+
+				return t;
+			}
+			return null;
 		}
 
 		private void add(Seen<T> seen) {
+			// Remove old entry, if one exists
 			Iterator<Seen<T>> it = findEntry(seen.snapshot);
 			if (it != null)
 				it.remove();
@@ -106,39 +136,38 @@ public class GameMap {
 		this.inView = inView;
 	}
 
-	public Action tick(List<Message> messages, List<Sugar> visibleSugar) {
+	private void updateState(List<Message> messages, List<Sugar> visibleSugar) {
 		for (Message message : messages)
 			if (message != null && message.contentSugar != null) {
-				int content = message.content;
-				Sugar sugar = message.contentSugar;
-				// The sign specifies if the Sugar is alive or dead
-				if (message.content <= 0)
-					sugarSeen.add(new Seen<Sugar>(sugar, tick + content, false));
-				else if (!sugarSeen.isKnown(message.contentSugar))
-					sugarSeen.add(new Seen<Sugar>(sugar, tick - content));
+				Seen<Sugar> seen = sugarSeen.getEntry(message.contentSugar);
+				if (seen != null)
+					seen.updateFromMessage(message.content);
+				else
+					sugarSeen.add(Seen.sugarSeenFromMessage(ticks, message.content, message.contentSugar));
 			}
 
-		sugarSeen.removeOld(tick - MAX_ALIVE_AGE, tick - MAX_DEAD_AGE);
+		sugarSeen.removeOld(ticks - MAX_ALIVE_AGE, ticks - MAX_DEAD_AGE);
 		if (inView != null) {
 			sugarSeen.handleVisible(inView, visibleSugar);
 		}
 		for (Sugar sugar : visibleSugar)
-			sugarSeen.add(new Seen<Sugar>(sugar, tick));
+			sugarSeen.add(new Seen<Sugar>(sugar, ticks));
+	}
 
-		tick++;
-
-		// Pick a message to broadcast
-		if (sugarSeen.snapshots.isEmpty())
+	private Action getAction() {
+		if (broadcastList == null || broadcastList.isEmpty()) {
+			broadcastList = new ArrayDeque<Seen<? extends Snapshot>>(sugarSeen.snapshots);
+		}
+		if (broadcastList == null || broadcastList.isEmpty())
 			return null;
 
-		sugarSeenMessageIndex++;
-		if (sugarSeenMessageIndex >= sugarSeen.snapshots.size())
-			sugarSeenMessageIndex = 0;
-		Seen<Sugar> sugar = sugarSeen.snapshots.get(sugarSeenMessageIndex);
-		int t = tick - sugar.tick;
-		if (!sugar.alive)
-			t *= -1;
-		return new Action(t, sugar.snapshot);
+		return broadcastList.poll().getAction(ticks);
+	}
+
+	public Action tick(List<Message> messages, List<Sugar> visibleSugar) {
+		updateState(messages, visibleSugar);
+		ticks++;
+		return getAction();
 	}
 
 	public Sugar getClosestSugarExcept(Vector position, Sugar except) {
