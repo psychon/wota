0120d70b47d97eefdd4a926ca4020d23892567df
Uli Schlachter <psychon@znc.in>
2014-03-25 21:24:42 +0100

    Hill: create Soldier if enemy is near
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- src/wota/ai/ameisenbaer/HillAI.java
+++ src/wota/ai/ameisenbaer/HillAI.java
@@ -4,6 +4,7 @@ import wota.gamemaster.AIInformation;
 import wota.gameobjects.Caste;
 import wota.gameobjects.Message;
 import wota.gameobjects.Sugar;
+import wota.gameobjects.Ant;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,22 +14,26 @@ public class HillAI extends wota.gameobjects.HillAI {
 	private final GameMap map = new GameMap(null);
 	private int tick = 0;
 
+	protected boolean haveVisibleEnemy() {
+		for (Ant ant : visibleAnts) {
+			if (ant.playerID != self.playerID) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public void tick() throws Exception {
 		double myfood = self.food;
-		while (parameters.ANT_COST < myfood) {
+		while (parameters.ANT_COST <= myfood) {
 			myfood -= parameters.ANT_COST;
 			tick++;
 
-			switch (tick % 10) {
-				case 8:
-				case 9:
-					createAnt(Caste.Soldier, AntAI.class);
-					break;
-				default:
-					createAnt(Caste.Gatherer, AntAI.class);
-					break;
-			}
+			if (tick % 20 > 15 || (haveVisibleEnemy() && tick % 2 == 0))
+				createAnt(Caste.Soldier, AntAI.class);
+			else
+				createAnt(Caste.Gatherer, AntAI.class);
 		}
 
 		Action mapAction = map.tick(new ArrayList<Message>(audibleAntMessages), Collections.<Sugar>emptyList());
